f3724786505f7a83f288d1040511e4fbdfc42811
IgorPelevanyuk <gavelock@gmail.com>
2013-11-24 22:02:07 +0400

    abstract spider added

---
+++ src/Spider.py
@@ -0,0 +1,11 @@
+class Spider:
+
+    def __init__(self, options = None):
+        self.__initialize() 
+
+    def __initialize(self, options = None):
+        pass
+
+    def analyse(self):
+        pass
+
